152d7d99dc6f532491a4f374cf6921030be331ce
shan wolff <shanwolff182@gmail.com>
2023-11-02 16:59:32 -0400

    Find Duplicates in progress

--- algorithms/3-Arrays-and-Objects/findDuplicates.js
+++ algorithms/3-Arrays-and-Objects/findDuplicates.js
@@ -1,7 +1,14 @@
 /**
-Take 45 minutes to complete this exercise. If you need more time, you can use Focus Friday.  
- 
 Write a JavaScript function that takes an array of integers as input and returns true if there are any duplicate elements in the array, and false otherwise.
+
+create:
+-function
+-array
+-integers
+    -input
+-returns
+    true: if duplicates
+    false: no duplicates
 
 Input:
     1. An array of integers.
@@ -12,15 +19,32 @@ Output:
 /**
 Steps:
 1. Create an empty object to store encountered numbers.
+    let encounteredNumbers = {};
 2. Iterate through the input array.
+    run through each input in the array
 3. For each number, check if it already exists in the object.
+    create a for loop to check if numbers exist within the object.
 4. If the number is found, there is a duplicate, so return true.
+    if statement: 
+        return true: if duplicate is found
 5. If the number is not found, add it to the object.
+    if statement:
+        if false add number to array
 6. After iterating through the entire array, if no duplicates are found, return false.
+    if statement:
+        return false if no duplicates found
  */
 
+
+
 function hasDuplicates(arr) {
-   
+    let encounteredNumbers = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        let numIteration = numbers1[i];
+        if (encounteredNumbers)
+            return true;
+    }
 }
 
 // Example usage
